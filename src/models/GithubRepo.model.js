e23059daaa1f9a4271bb213d8fa5569efb96c937
Washington Yandun <washington.yandun.morales@gmail.com>
2024-06-17 11:50:46 -0500

    refactor: Update model

--- src/models/GithubRepo.model.js
+++ src/models/GithubRepo.model.js
@@ -1,20 +1,11 @@
 import { ConnectionError, FetchError, ForbiddenError } from "../errors/ErrorFactory.js";
 
-/**
- * GithubRepo model class
- * @class GithubRepo
- */
 export class GithubRepo {
-    /**
-     * Create a GithubRepo.
-     * @param {GithubRepoBuilder} builder - The builder object containing all properties.
-     */
     constructor(builder) {
         this.name = builder.name;
 
         this.description = builder.description;
         this.license = builder.license;
-        this.topics = builder.topics;
         this.htmlUrl = builder.htmlUrl;
 
         this.starsCount = builder.starsCount;
@@ -22,56 +13,13 @@ export class GithubRepo {
         this.watchersCount = builder.watchersCount;
 
         this.language = builder.language;
-        this.languagesUrl = builder.languagesUrl;
 
         this.createdAt = new Date(builder.createdAt);
         this.updatedAt = new Date(builder.updatedAt);
         this.pushedAt = new Date(builder.pushedAt);
-
-        this.langStats = {};
-        this.timeStats = {};
     }
-
-    /**
-     * Set the language statistics.
-     * @param {Object} langStats - The language statistics object.
-     */
-    async setLangStats() {
-        // get languages stats from languagesUrl
-        try {
-            const response = await fetch(this.languagesUrl, {
-                method: "GET",
-                headers: {
-                    Accept: "application/vnd.github.v3+json",
-                },
-            });
-
-            if (!response.ok) {
-                throw new ConnectionError("Failed to fetch data");
-            } else if (response.status === 403) {
-                throw new ForbiddenError("Forbidden to fetch Github data");
-            }
-
-            const langStats = await response.json();
-            this.langStats = langStats;
-        } catch (error) {
-            throw new FetchError(error.message);
-        }
-    }
-
-    /**
-     * Set the time statistics.
-     * @param {Object} timeStats - The time statistics object.
-     */
-
-    setTimeStats() {}
 }
 
-/**
- * Class used to build a GithubRepo object.
- *
- * @class GithubRepoBuilder
- */
 export class GithubRepoBuilder {
     setFullName(name) {
         this.name = name || "N/A";
@@ -83,10 +31,6 @@ export class GithubRepoBuilder {
     }
     setLicense(license) {
         this.license = license ? license.name : "N/A";
-        return this;
-    }
-    setTopics(topics) {
-        this.topics = topics || ["N/A"];
         return this;
     }
     setHtmlUrl(htmlUrl) {
@@ -107,10 +51,6 @@ export class GithubRepoBuilder {
     }
     setLanguage(language) {
         this.language = language || "N/A";
-        return this;
-    }
-    setLanguagesUrl(languagesUrl) {
-        this.languagesUrl = languagesUrl || "N/A";
         return this;
     }
     setCreatedAt(createdAt) {
