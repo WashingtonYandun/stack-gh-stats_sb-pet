75f18d6e711912dc9aed42ca6dd1338142ca39cb
Washington Yandun <washington.yandun.morales@gmail.com>
2024-06-18 11:16:46 -0500

    test: Create the tests

--- test/unit/index.test.js
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest";
-
-import { getGithubRepos } from "../../src/helpers/Core.helper.js";
-
-test("getGithubRepos should return an array of repositories", async () => {
-    const repos = await getGithubRepos();
-    expect(Array.isArray(repos)).toBe(true);
-});
